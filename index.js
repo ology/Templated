df5c15b8b87527711092935a49eb51b92d6cee53
Gene Boggs <gene@ology.net>
2022-01-02 09:14:01 -0800

    Fix comment

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ let sess; // global session for demo purposes
 app.use(bodyParser.json());      
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// global db - oof!
+// global db!
 let db;
 (async () => {
     db = await open({
